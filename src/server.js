7f9e650ca2975ef6fee2b6cc0763ad48aaf22858
filipejacomozzi <filipe.jacomozzi@benner.com.br>
2024-08-23 16:42:17 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 897e5d621117b8f820ec31d19412a82f54053c81
+  console.log("API disponível em http://localhost:8000")
 })
